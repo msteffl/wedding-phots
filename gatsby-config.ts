7fb071d20831079960704f05132742203c11b201
smf2abt <michael.steffl2@bosch.com>
2022-05-15 14:26:18 +0200

    Initial commit from gatsby

---
+++ gatsby-config.ts
@@ -0,0 +1,11 @@
+import type { GatsbyConfig } from "gatsby";
+
+const config: GatsbyConfig = {
+  siteMetadata: {
+    title: `wedding-photos`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-sass"]
+};
+
+export default config;
